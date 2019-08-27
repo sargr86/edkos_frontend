66c9fb8ee4772089c5fcac1c79b5d06f95adb634
Aleksandr Ivanov <aleksandr.ivanov.77119@gmail.com>
2019-08-27 16:14:49 +0400

    Added login page to its component, also added shared module and main styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {IndexComponent} from './core/index/index.component';
+import {LoginComponent} from './core/login/login.component';
 
 const routes: Routes = [
     {
         path: '',
         component: IndexComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
     }
 ];
 
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IndexComponent } from './index/index.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, LoginComponent],
   imports: [
     CommonModule
   ]
