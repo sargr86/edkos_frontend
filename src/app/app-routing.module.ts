cb256e8b70669de420027cf7147086370b99a4b7
Aleksandr Ivanov <aleksandr.ivanov.77119@gmail.com>
2019-08-27 15:55:28 +0400

    Installed angular project and added index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {IndexComponent} from './core/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: IndexComponent
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
